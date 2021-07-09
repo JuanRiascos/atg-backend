25b976b5b676aff9009a30d7f87709c4b2697bac
darian7 <yoj.cabrera@udla.edu.co>
2021-07-09 00:33:57 -0500

    feat versions entity

---
+++ src/entities/utils/versions.entity.ts
@@ -0,0 +1,21 @@
+import { 
+  Column, 
+  Entity, 
+  PrimaryGeneratedColumn
+ } from "typeorm";
+
+@Entity("versions", { schema: 'utils' })
+export class Versions {
+
+    @PrimaryGeneratedColumn({ type: "bigint" })
+    id: number;
+
+    @Column("character varying", { name: 'apple_version', length: 20, nullable: true })
+    appleVersion: string;
+    
+    @Column("character varying", { name: 'expo_version', length: 20, nullable: true })
+    expoVersion: string;
+    
+    @Column("character varying", { name: 'expo_build', length: 20, nullable: true })
+    expoBuild: string;
+}
