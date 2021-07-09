c0ed05b297635d583b429c15a09e4213efec4287
darian7 <yoj.cabrera@udla.edu.co>
2021-07-09 00:35:00 -0500

    refactor code

--- src/entities/utils/versions.entity.ts
+++ src/entities/utils/versions.entity.ts
@@ -1,21 +1,20 @@
-import { 
-  Column, 
-  Entity, 
+import {
+  Column,
+  Entity,
   PrimaryGeneratedColumn
- } from "typeorm";
-
+} from "typeorm";
 @Entity("versions", { schema: 'utils' })
 export class Versions {
 
-    @PrimaryGeneratedColumn({ type: "bigint" })
-    id: number;
+  @PrimaryGeneratedColumn({ type: "bigint" })
+  id: number;
 
-    @Column("character varying", { name: 'apple_version', length: 20, nullable: true })
-    appleVersion: string;
-    
-    @Column("character varying", { name: 'expo_version', length: 20, nullable: true })
-    expoVersion: string;
-    
-    @Column("character varying", { name: 'expo_build', length: 20, nullable: true })
-    expoBuild: string;
+  @Column("character varying", { name: 'apple_version', length: 20, nullable: true })
+  appleVersion: string;
+
+  @Column("character varying", { name: 'expo_version', length: 20, nullable: true })
+  expoVersion: string;
+
+  @Column("character varying", { name: 'expo_build', length: 20, nullable: true })
+  expoBuild: string;
 }
