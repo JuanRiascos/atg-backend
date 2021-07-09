a726f239ed943e8cd491a7277e2d02ccc776bc43
fabiae <yei.claros@udla.edu.co>
2021-07-09 00:32:45 -0500

    feat: export data in excel

--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -8,10 +8,13 @@ import { Client } from 'src/entities/client/client.entity';
 import { StatisticService } from './services/statistic.service';
 import { SessionClient } from 'src/entities/client/session-client.entity';
 import { SessionService } from './services/session.service';
+import { ViewCaseStudies } from 'src/entities/academy/views-case-studies.entity';
+import { ViewExtraReps } from 'src/entities/academy/views-extra-reps.entity';
+import { ViewVideos } from 'src/entities/academy/views-videos.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Interest, Client, SessionClient])
+    TypeOrmModule.forFeature([Interest, Client, SessionClient, ViewCaseStudies, ViewExtraReps, ViewVideos])
   ],
   controllers: [ClientController],
   providers: [InterestService, StatisticService, SessionService]
--- src/modules/client/services/statistic.service.ts
+++ src/modules/client/services/statistic.service.ts
@@ -3,13 +3,21 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { States, StateSubscription } from "src/entities/@enums/index.enum";
 import { Client } from "src/entities/client/client.entity";
 import { Repository } from "typeorm";
+import * as XLSX from 'xlsx';
 import moment = require('moment');
+import { ViewCaseStudies } from "src/entities/academy/views-case-studies.entity";
+import { ViewExtraReps } from "src/entities/academy/views-extra-reps.entity";
+import { ViewVideos } from "src/entities/academy/views-videos.entity";
+
 
 @Injectable()
 export class StatisticService {
 
   constructor(
-    @InjectRepository(Client) private readonly clientRepository: Repository<Client>
+    @InjectRepository(Client) private readonly clientRepository: Repository<Client>,
+    @InjectRepository(ViewCaseStudies) private readonly viewCaseRepository: Repository<ViewCaseStudies>,
+    @InjectRepository(ViewExtraReps) private readonly viewExtraRepository: Repository<ViewExtraReps>,
+    @InjectRepository(ViewVideos) private readonly viewVideosRepository: Repository<ViewVideos>
   ) { }
 
   async getRegisteredClients() {
@@ -77,5 +85,113 @@ export class StatisticService {
     }
 
     return result
+  }
+
+  async getReportData() {
+    const clients = await this.clientRepository.createQueryBuilder('client')
+      .select(['client.id', 'client.city'])
+      .addSelect(['user.id', 'user.email', 'user.createdAt'])
+      .addSelect(['person.name', 'person.lastname', 'person.phone', 'person.positionCurrentJob'])
+      .addSelect(['ocupation.name'])
+      .addSelect(['sport.name'])
+      .innerJoin('client.user', 'user')
+      .innerJoin('user.person', 'person')
+      .leftJoin('person.ocupation', 'ocupation')
+      .leftJoin('person.sport', 'sport')
+      .addOrderBy('user.createdAt')
+      .getMany()
+
+    let fileName = 'users-data.xlsx'
+
+    let data = clients.map(client => {
+      return [
+        client.id, `${client.user.person.name} ${client.user.person.lastname}`,
+        client.user.email, client.user.createdAt, client.user.person.phone, client.city?.name,
+        client.user.person.ocupation?.name, client.user.person.sport?.name,
+        client.user.person.positionCurrentJob
+      ]
+    })
+
+    var ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
+      [
+        'Client Id', 'Name and Lastname', 'Email',
+        'Register Date', 'Phone', 'City', 'Ocupation', 'Sport', 'Position/Current Job'
+      ],
+      ...data
+    ]);
+    var wb: XLSX.WorkBook = { Sheets: { 'UsersData': ws }, SheetNames: ['UsersData'] };
+    var file = XLSX.writeFile(wb, fileName, { bookType: 'xlsx' })
+
+    return { file, fileName }
+  }
+
+  async getContenView() {
+    const videos = await this.viewVideosRepository.createQueryBuilder('view')
+      .select(['view.date'])
+      .addSelect(['video.title', 'course.title', 'client.id'])
+      .innerJoin('view.video', 'video')
+      .innerJoin('video.course', 'course')
+      .innerJoin('view.client', 'client')
+      .where('view.first = true')
+      .orderBy('view.date')
+      .getMany()
+
+    const extras = await this.viewExtraRepository.createQueryBuilder('view')
+      .select(['view.date'])
+      .addSelect(['extraRep.title', 'course.title', 'client.id'])
+      .innerJoin('view.extraRep', 'extraRep')
+      .innerJoin('extraRep.course', 'course')
+      .innerJoin('view.client', 'client')
+      .where('view.first = true')
+      .orderBy('view.date')
+      .getMany()
+
+    const cases = await this.viewCaseRepository.createQueryBuilder('view')
+      .select(['view.date'])
+      .addSelect(['caseStudy.title', 'course.title', 'client.id'])
+      .innerJoin('view.caseStudy', 'caseStudy')
+      .innerJoin('caseStudy.course', 'course')
+      .innerJoin('view.client', 'client')
+      .where('view.first = true')
+      .orderBy('view.date')
+      .getMany()
+
+    let dataVideos = videos.map(item => {
+      return [
+        item.video.course.title, item.video.title, 'Video',
+        moment(item.date).format('DD MMM YYYY'), moment(item.date).format('LT'),
+        item.client.id
+      ]
+    })
+
+    let dataExtras = extras.map(item => {
+      return [
+        item.extraRep.course.title, item.extraRep.title, 'Extra Rep',
+        moment(item.date).format('DD MMM YYYY'), moment(item.date).format('LT'),
+        item.client.id
+      ]
+    })
+
+    let dataCases = cases.map(item => {
+      return [
+        item.caseStudy.course.title, item.caseStudy.title, 'Case Study',
+        moment(item.date).format('DD MMM YYYY'), moment(item.date).format('LT'),
+        item.client.id
+      ]
+    })
+
+    let fileName = 'viewed-content.xlsx'
+    var ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
+      [
+        'Categorie', 'Name', 'Content Type', 'Date Viewed', 'Time Viewed', 'Client Id'
+      ],
+      ...dataVideos,
+      ...dataExtras,
+      ...dataCases
+    ]);
+    var wb: XLSX.WorkBook = { Sheets: { 'ViewedContent': ws }, SheetNames: ['ViewedContent'] };
+    var file = XLSX.writeFile(wb, fileName, { bookType: 'xlsx' })
+
+    return { file, fileName }
   }
 }
