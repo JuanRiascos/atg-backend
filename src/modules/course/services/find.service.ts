e6d0c735bb96e51d1d2e0c9939981e9bbd2f6a05
fabiae <yei.claros@udla.edu.co>
2021-07-21 15:24:26 -0500

    refactor: optimized query course

--- src/modules/course/services/find.service.ts
+++ src/modules/course/services/find.service.ts
@@ -29,50 +29,60 @@ export class FindService {
   async getCourse(courseId: number, clientId: number) {
     let course
     try {
-      course = await this.courseRepository.createQueryBuilder('course')
-        .select(['course.id', 'course.title', 'course.subtitle', 'course.cover',
+      let query = this.courseRepository.createQueryBuilder('course')
+        .select(['course.id', 'course.title', 'course.subtitle',
           'course.color', 'course.image', 'course.iconReps', 'course.iconCases'])
         .leftJoinAndSelect('course.extraReps', 'extraReps')
-        .leftJoinAndSelect('course.assessments', 'assessments')
-        .leftJoinAndSelect('assessments.questions', 'questions')
-        .leftJoinAndSelect('questions.answers', 'answers')
         .leftJoinAndSelect('course.videos', 'videos')
-        .leftJoinAndSelect('videos.clients', 'client', 'client.id = :clientId', { clientId })
         .leftJoinAndSelect('course.caseStudies', 'caseStudies')
-        .leftJoinAndSelect('caseStudies.clients', 'clientCaseStudies', 'clientCaseStudies.id = :clientId', { clientId })
-        .leftJoinAndSelect('extraReps.clients', 'clientExtraReps', 'clientExtraReps.id = :clientId', { clientId })
-        .where('course.id = :courseId', { courseId })
+        .leftJoinAndSelect('videos.checks', 'checks')
+      if (!clientId) {
+        query.leftJoinAndSelect('course.assessments', 'assessments')
+          .leftJoinAndSelect('assessments.questions', 'questions')
+          .leftJoinAndSelect('questions.answers', 'answers')
+      }
+      if (clientId) {
+        query.leftJoinAndSelect('extraReps.clients', 'clientExtraReps', 'clientExtraReps.id = :clientId', { clientId })
+          .leftJoinAndSelect('caseStudies.clients', 'clientCaseStudies', 'clientCaseStudies.id = :clientId', { clientId })
+          .leftJoinAndSelect('videos.clients', 'client', 'client.id = :clientId', { clientId })
+          .leftJoinAndSelect('checks.clients', 'clients', 'client.id = :clientId', { clientId })
+      }
+      query.where('course.id = :courseId', { courseId })
         .addOrderBy('caseStudies.order', 'ASC')
         .addOrderBy('extraReps.order', 'ASC')
         .addOrderBy('videos.order', 'ASC')
-        .addOrderBy('questions.order', 'ASC')
-        .addOrderBy('answers.order', 'ASC')
-        .getOne()
+        .addOrderBy('checks.order', 'ASC')
+      if (!clientId) {
+        query.addOrderBy('questions.order', 'ASC')
+          .addOrderBy('answers.order', 'ASC')
+      }
+
+      course = await query.getOne()
     } catch (error) {
       return { error }
     }
 
     if (!course)
       return { error: 'NOT_FOUND' }
-
-    for (const video of course?.videos) {
-      if (video.url) {
-        let response
-        try {
-          response = await this.httpService.get(
-            `https://player.vimeo.com/video/${video?.url?.replace("https://vimeo.com/", "")}/config`
-          ).toPromise()
-        } catch (error) {
-
+    /* 
+        for (const video of course?.videos) {
+          if (video.url) {
+            let response
+            try {
+              response = await this.httpService.get(
+                `https://player.vimeo.com/video/${video?.url?.replace("https://vimeo.com/", "")}/config`
+              ).toPromise()
+            } catch (error) {
+    
+            }
+    
+            const data = await response?.data
+            let urlVimeo = data?.request?.files?.hls?.cdns?.akfire_interconnect_quic?.url
+    
+            video['urlVimeo'] = urlVimeo || video?.url
+          }
         }
-
-        const data = await response?.data
-        let urlVimeo = data?.request?.files?.hls?.cdns?.akfire_interconnect_quic?.url
-
-        video['urlVimeo'] = urlVimeo || video?.url
-      }
-    }
-
+     */
 
     return course
   }
