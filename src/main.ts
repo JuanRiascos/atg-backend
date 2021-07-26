ce05db87f886656ffba6e56646372bf015ab644f
darian7 <yoj.cabrera@udla.edu.co>
2021-07-25 23:49:32 -0500

    feat(utilis): send email resource

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   app.setGlobalPrefix(prefix)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
   await app.listen(port);
 
--- src/modules/utils/services/email.service.ts
+++ src/modules/utils/services/email.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-
+const fs = require("fs");
 import { Templates } from 'src/@common/services/sendgrid.service';
 
 @Injectable()
@@ -9,12 +9,26 @@ export class EmailService {
     @Inject('SendgridService') private readonly sendgridService,
   ) { }
 
-  async sendEmail(user, body) {
-    
+  async sendEmailFeedback(user, body) {
+
     await this.sendgridService.sendEmail(
       "info@athletetechgroup.com",
       Templates.FEEDBACK,
       { ...body, email: user?.email }
+    )
+
+    return { success: "ok" }
+  }
+
+  async sendEmailAttachments(user, body) {
+
+    const attachments = [body]
+
+    await this.sendgridService.sendEmail(
+      "darian7cc@gmail.com",
+      Templates.FEEDBACK,
+      { ...body, email: user?.email },
+      attachments
     )
 
     return { success: "ok" }
